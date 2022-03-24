39c705f6f8ff06471308f5bd95565b5ccf084b73
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-03-24 12:17:44 +0900

    week 4

---
+++ ArrayList.java
@@ -0,0 +1,60 @@
+package com.company;
+import java.util.Arrays;
+
+public class ArrayList<E> implements List<E>{
+    static int DefaultSize = 10;
+    E[] data;
+    int listSize;
+
+    public ArrayList(){
+        this(DefaultSize);
+    }
+    public ArrayList(int size){
+        listSize = 0;
+        data = (E[]) new Object[size];
+    }
+
+    public void clear(){
+        listSize = 0;
+    }
+    public void insert(int pos, E item){
+        for (int i=listSize; i>pos; i--){
+            data[i] = data[i-1];
+        }
+        data[pos] = item;
+        listSize++;
+    }
+    public void append(E item){
+        data[listSize++] = item;
+    }
+    public void update(int pos, E item){
+        data[pos] = item;
+    }
+    public E getValue(int pos){
+        return data[pos];
+    }
+    public E remove(int pos){
+        E ret = data[pos];
+        listSize--;
+        for (int i=pos; i<listSize; i++){
+            data[i] = data[i+1];
+        }
+        return ret;
+    }
+    public int length(){
+        return listSize;
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.append(3);
+        System.out.println(Arrays.toString(myList.data));
+        myList.append(5);
+        System.out.println(Arrays.toString(myList.data));
+        myList.insert(0,10);
+        System.out.println(Arrays.toString(myList.data));
+        myList.remove(2);
+        System.out.println(Arrays.toString(myList.data));
+        System.out.println(myList.length());
+    }
+}
