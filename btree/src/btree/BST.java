29625ec829945553e340366bb41bc70ac79c72f1
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-04-28 14:14:21 +0900

    BST

--- btree/src/btree/BST.java
+++ btree/src/btree/BST.java
@@ -1,20 +1,106 @@
 package btree;
 
-public class BST<K, E> implements Dictionary<K, E>{
+public class BST<K extends Comparable<K>, E> implements Dictionary<K, E>{
+
+    class Entry{
+        public K key;
+        public E element;
+        public Entry(K key, E element){
+            this.key = key;
+            this.element = element;
+        }
+    }
+
+    private BinNode<Entry> root;
+    private int size;
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
 
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
     }
 
     @Override
     public void insert(K k, E e) {
+        root = insert_helper(k, e, root);
+        size++;
+    }
 
+    public BinNode<Entry> insert_helper(K k, E e, BinNode<Entry> rt){
+        if (rt == null){
+            return new INode<>(new Entry(k, e), null, null);
+        }
+        else if (rt.element().key.compareTo(k) == 0){
+            rt.element().element = e;
+        }
+        else if(rt.element().key.compareTo(k) < 0){
+            rt.setRight(insert_helper(k, e, rt.right()));
+        }
+        else {
+            rt.setLeft(insert_helper(k, e, rt.left()));
+        }
+        return rt;
     }
 
     @Override
     public E remove(K k) {
-        return null;
+        E ret = find_helper(k, root);
+        if (ret != null){
+            root = remove_helper(k, root);
+            size--;
+        }
+        return ret;
+    }
+
+    private BinNode<Entry> remove_helper (K k, BinNode<Entry> rt){
+        if (rt.element().key.compareTo(k) > 0){
+            rt.setLeft(remove_helper(k, rt.left()));
+        }
+        else if (rt.element().key.compareTo(k) < 0){
+            rt.setRight(remove_helper(k, rt.right()));
+        }
+        else {
+//            if (rt.left() == null && rt.right() == null){
+//                return null;
+//            }
+//            else
+            if (rt.left() == null){
+                return rt.right();
+            }
+            else if (rt.right() == null){
+                return rt.left();
+            }
+            else{
+                Entry leftMost = getLeftMost(rt.right());
+                rt.setElement(leftMost);
+                rt.setRight(removeLeftMost(rt.right()));
+                return rt;
+            }
+        }
+        return rt;
+    }
+
+    private Entry getLeftMost(BinNode<Entry> rt){
+        BinNode<Entry> cur = rt;
+        while(cur.left() != null){
+            cur = cur.left();
+        }
+        return cur.element();
+    }
+
+    private BinNode<Entry> removeLeftMost(BinNode<Entry> rt){
+        if(rt.left() == null){
+            return rt.right();
+        }
+        else {
+            rt.setLeft(removeLeftMost(rt.left()));
+            return rt;
+        }
     }
 
     @Override
@@ -24,11 +110,25 @@ public class BST<K, E> implements Dictionary<K, E>{
 
     @Override
     public E find(K k) {
-        return null;
+        return find_helper(k, root);
+    }
+    public E find_helper (K k, BinNode<Entry> rt){
+        if (rt == null) {
+            return null;
+        }
+        else if (rt.element().key.compareTo(k) == 0) {
+            return rt.element().element;
+        }
+        else if (rt.element().key.compareTo(k) < 0) {
+            return find_helper(k, rt.right());
+        }
+        else {
+            return find_helper(k, rt.left());
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
---
+++ btree/src/btree/BSTTest.java
@@ -0,0 +1,21 @@
+package btree;
+
+public class BSTTest {
+    public static void main(String[] args){
+
+        Dictionary<Integer, String> bst = new BST<>();
+
+        bst.insert(11,"a");
+        bst.insert(3, "b");
+        bst.insert(5, "c");
+        bst.insert(2, "d");
+        bst.remove(3);
+
+        System.out.println(bst.find(2));
+        System.out.println(bst.find(5));
+        System.out.println(bst.find(3));
+        System.out.println(bst.find(11));
+        System.out.println(bst.find(30));
+
+    }
+}
