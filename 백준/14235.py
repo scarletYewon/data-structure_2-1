1a76133bb259b6c813a36b6af2a41ce4f512b884
이예원 <81704418+scarletYewon@users.noreply.github.com>
2022-06-20 17:54:56 +0900

    Update 14235.py

--- 백준/14235.py
+++ 백준/14235.py
@@ -4,7 +4,7 @@ import sys
 t = int(input())
 hq = []
 for i in range(t):
-    num =  list(map(int, sys.stdin.readline().split()))
+    num = list(map(int, sys.stdin.readline().split()))
     if num[0] == 0:
         if len(hq)!=0:
             print(heapq.heappop(hq)[1])
